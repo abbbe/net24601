9ad91548d267f4ac0b1703b10308341f81df1afb
Alexandre Bezroutchko <abb@gremwell.com>
2018-01-27 00:18:23 +0100

    works

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+./net42.sh (run|fund|attach)
+```
+
+* run - start mining on a private network (optionally initialize from genesis block and create 4 unlocked accounts)
+* fund - spread mined funds across 4 accounts
+* attach - attach to console
+
--- net42.sh
+++ net42.sh
@@ -7,22 +7,17 @@ GENESIS=$BASEDIR/genesis.json
 GETH_ARGS="--datadir $DATADIR --nodiscover --maxpeers 0 --targetgaslimit 7500000 --rpc"
 
 case $1 in
-	init)
-		geth --datadir $DATADIR init $GENESIS
-		geth $GETH_ARGS --exec "loadScript('$BASEDIR/init_accounts.js')" console
-		$0 run
-		;;
-
 	run)
+		if [ ! -d $DATADIR ] ; then
+			geth --datadir $DATADIR init $GENESIS
+			geth $GETH_ARGS --exec "loadScript('$BASEDIR/init_accounts.js')" console
+		fi
+
 		geth $GETH_ARGS --mine --minerthreads 1 --unlock 0,1,2,3 --password /dev/null console
 		;;
 
 	fund)
 		geth $GETH_ARGS --exec "loadScript('$BASEDIR/fund_accounts.js')" attach $IPCPATH
-		;;
-
-	removedb)
-		geth --datadir $DATADIR removedb
 		;;
 
 	attach)
